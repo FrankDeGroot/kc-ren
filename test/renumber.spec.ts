1288570daaa1def99d068e0d7a808133d673bb2d
Frank de Groot - Schouten <frank.de.groot@ing.nl>
2017-12-21 08:37:54 +0100

    Test renumbering and skipping

--- test/renumber.spec.ts
+++ test/renumber.spec.ts
@@ -8,5 +8,23 @@ describe('renumber', () => {
 	});
 	it('should disregard files without a number prefix', () => {
 		expect(renumber(['a', 'b'])).to.be.an('array').that.is.empty;
+	});
+	it('should renumber', () => {
+		expect(renumber(['1-x', '2-a', '3-@'])).to.eql([{
+			oldFile: '3-@',
+			newFile: '030-@'
+		}, {
+			oldFile: '2-a',
+			newFile: '020-a'
+		}, {
+			oldFile: '1-x',
+			newFile: '010-x'
+		}]);
+	});
+	it('should skip files that are not renamed', () => {
+		expect(renumber(['1-x', '020-a'])).to.eql([{
+			oldFile: '1-x',
+			newFile: '010-x'
+		}]);
 	})
 });
