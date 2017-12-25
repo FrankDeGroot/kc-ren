c30e2ae15627112fc6f8808c15b6b8cd4dbe3c94
Frank de Groot - Schouten <frank.de.groot@ing.nl>
2017-12-25 13:30:33 +0100

    + test rename files

--- test/renumberDir.spec.ts
+++ test/renumberDir.spec.ts
@@ -1,19 +1,36 @@
 import { renumberDir } from '../src/index';
 
 import { expect } from 'chai';
-import tmp = require('tmp');
+import * as fs from 'fs';
+import * as path from 'path';
+import * as rimraf from 'rimraf';
+import * as tmp from 'tmp';
 
 describe('renumberDir', () => {
 	it('should handle an empty directory', () => {
 		inTmpDir(renumberDir);
 	});
+	it('should rename the file', () => {
+		inTmpDir(dir => {
+			createFile(dir, '1-a');
+			renumberDir(dir);
+			const files = fs.readdirSync(dir);
+			expect(files).to.deep.equal(['010-a']);
+		});
+	});
 });
 
 function inTmpDir(doTest: (string) => void) {
-		const tmpDir = tmp.dirSync();
-		try {
-			doTest(tmpDir.name);
-		} finally {
+	const tmpDir = tmp.dirSync();
+	try {
+		doTest(tmpDir.name);
+	} finally {
+		rimraf(tmpDir.name, () => {
 			tmpDir.removeCallback();
-		}
+		});
+	}
+}
+
+function createFile(dir: string, name: string) {
+	fs.writeFileSync(path.join(dir, name), '');
 }
