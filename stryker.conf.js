855cb967e21f1224594f70edb0243a282ebeb214
Frank de Groot - Schouten <frank.de.groot@ing.nl>
2018-01-02 20:06:07 +0100

    Stryker mutates TypeScript

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,14 +1,17 @@
 module.exports = function(config) {
   config.set({
     files: [
-      // Add your files here, this is just an example:
-      { pattern: 'src/**/*.js', mutated: true, included: false },
-      'src/index.js',
-      'test/**/*.js'
+      { pattern: 'src/**/*.ts', mutated: true },
+      'test/**/*.ts',
     ],
+    tsconfigFile: 'tsconfig.json',
+    transpilers: [
+      'typescript'
+    ],
+    mutator: 'typescript',
     testRunner: 'mocha',
     testFramework: 'mocha',
-    coverageAnalysis: 'all',
+    coverageAnalysis: 'off',
     reporter: ['clear-text', 'html', 'progress', 'baseline']
   });
 }
